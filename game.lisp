b8a097302c70a0940ff6b24d4be6ba4d29716020
ade-sede <adrien.de.sede@gmail.com>
2018-03-03 20:38:35 +0100

    Added a few minor things. removed useless lines

--- game.lisp
+++ game.lisp
@@ -1,12 +1,6 @@
 (load "sdl.lisp")
 (load "variables.lisp")
 
-(defun ggg (y x) (
-				 if (not (ignore-errors(aref current_grid y x)))
-					  -1
-					  1 
-				  ))
-
 (defun get_sum (y x) (
 					  let ((sum 0))
 					   (loop for j from (- y 1) to (+ y 1) ; Iterate over lines
@@ -14,15 +8,21 @@
 								do (
 									if (and
 										(not (and (= j y) (= i x))) ; Not on the current cell
-										;; (= (ignore-errors (aref current_grid j i)) 1) ; && Neighbor is alive
-										(= (ggg j i) 1) ;; need correction
+										(< -1 j)					; Out of bounds check
+										(< -1 i)
+										(< j N)
+										(< i M)
+										(= (ignore-errors (aref current_grid j i)) 1) ; && Neighbor is alive
 										) 
 									   (setq sum (+ sum 1)) ; Then Add 1 to alive neighbor count
 									   ;; () ; else statement
-									   
 									   )))
 					   sum
 					   ))
+
+(defun my_debug (sum y x) (
+						   format t "~% Cell ~D ~D has a sum of ~D" x y sum
+								  ))
 
 (defun game () (
 				;;; For each cell, compute the number of alive
@@ -34,18 +34,21 @@
 				;;; Else -> State unchanged next turn
 
 				let ((sum 0))
+				 (format t "~% Current grid when entering the game ~% -------------------- ~% ~A" current_grid)
 				 (loop for y from 0 to (- N 1)
 					do (loop for x from 0 to (- M 1)
 						  do (
 							  let ()
 							   (setq sum (get_sum y x)) ; Number of alive neighboor
+							   ;; (my_debug sum y x)
 							   (setf (aref next_grid y x) (cond ; Select new state
-															((or (< 2 sum) (> 3 sum)) (draw y x DEAD) 0)
+															((or (> 2 sum) (< 3 sum)) (draw y x DEAD) 0)
 															((= sum 3) (draw y x ALIVE) 1)
 															(t (aref current_grid y x))
 															)
 									 )
 							   )))
+				 ;; (format t "~% Next grid when exiting the game ~% -------------------- ~% ~A" next_grid)
 				 (setq current_grid next_grid)
 				 )) 
 
--- main.lisp
+++ main.lisp
@@ -20,12 +20,7 @@
 					))
 
 (defun main (argv)(
-					let ()
-					(setq N (parse-integer (nth 1 *posix-argv*)))
-					(setq M (parse-integer (nth 2 *posix-argv*)))
-					;; SDL stuff
-					;;(setq size (/ (max (N M)) WIDTH))
-
+				   let ()
 					;; Update grid dimensions
 					(setq M (parse-integer (nth 1 *posix-argv*)))
 					(setq N (parse-integer (nth 2 *posix-argv*)))
@@ -34,15 +29,12 @@
 					(setq current_grid (make-array (list N M)))
 					(setq next_grid (make-array (list N M)))
 
-					;; Dummy cells
-					(setf (aref current_grid (- N 1) (- M 1)) 1)
-
 					(setq size (/ WIDTH (max N M)))
 					(setq csize size)
 					(sdl:with-init ()
 					  (sdl:window width height :title-caption "Carniflex")
 					  (sdl:update-display)
-						(setf (sdl:frame-rate) IMAGE_PER_SEC)
+					  (setf (sdl:frame-rate) IMAGE_PER_SEC)
 					  (sdl:with-events ()
 						(:quit-event () t)
 						(:key-down-event (:key key) (handle-key key))
@@ -50,7 +42,7 @@
 							   (sdl:clear-display COLOR_BACKGROUND)
 							   (game)
 							   (sdl:update-display)
-							)
+							   )
 						))
 					))
 
