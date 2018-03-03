8078bf3dc2a35bf1b9cfe5ce0fe43f57805ab711
bcozic <bcozic@student.42.fr>
2018-03-03 18:18:39 +0100

    check cell alive around

--- game.lisp
+++ game.lisp
@@ -1,8 +1,37 @@
 (load "sdl.lisp")
 (load "variables.lisp")
 
+(defun check_life (y x) (
+	(defvar i 0)
+	(defvar live 0)
+	(loop
+		(defvar j 0)
+		(setq i (+ i 1))
+			(loop
+				(setq j (+ j 1))
+				(if (and (not (and (= i y) (= j x)))
+					(and (/= (ignore-errors (aref current_grid i j)) NIL)
+						(= (ignore-errors (aref current_grid i j)) 1)))
+						(setq live (+ live 1)))
+	)))
+	(return live)
+)
 (defun game (A) (
 	let ()
+	(defvar x)
+	(defvar y)
+	(setq y 0)
+	(loop
+		(setq x 0)
+		(setq y (+ y 1))
+		(loop
+			(setq x (+ x 1))
+			(write )
+			(terpri)
+			(when (> x N) (return x))
+		)
+		(when (> y M) (return y))
+	)
 	;;ici on image la boucle qui check dans le tableau A
 	;;et decide de draw ou pas
 
