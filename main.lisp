2aeb25c6ae45fbb26a602db4814b058c3ee68326
ade-sede <adrien.de.sede@gmail.com>
2018-03-03 17:37:25 +0100

    Merged arg parsing with sdl main

--- main.lisp
+++ main.lisp
@@ -1,28 +1,49 @@
 (load "~/.sbcl/quicklisp/setup.lisp")
 (ql:quickload "lispbuilder-sdl")
-
 (load "variables.lisp")
 (load "sdl.lisp")
 (load "game.lisp")
 
-(defun main (argv)(
-	let ()
-	;; SDL stuff
-	(sdl:with-init ()
-	(sdl:window width height :title-caption "Carniflex")
-	(sdl:update-display)
 
-  (sdl:with-events ()
-		(:quit-event () t)
-		(:key-down-event (:key key) (handle-key key))
-		(:idle ()
-			(sdl:clear-display sdl:*black*)
-			(game 0)
-			(sdl:update-display)
-			(sleep 1)
-		)
-	))
-))
+(defparameter *usage-string* "Usage: sbcl --script main.lisp width height
+
+With:	width, non-zero positiv integer, the width of the grid
+		height, non-zero positiv integer, the height of the grid
+
+Note: Integers are 'one-indexed'")
+
+
+(defun parse () (if (or	
+					  (not *posix-argv*) ; No argv
+					  (/= (list-length *posix-argv*) 3) ; Not exactly 2 arguments
+					  (not (and ; Arguments are not positiv integers
+							(typep (ignore-errors (parse-integer (nth 1 *posix-argv*))) 'integer) 
+							(typep (ignore-errors (parse-integer (nth 2 *posix-argv*))) 'integer)
+							(< 0 (ignore-errors (parse-integer (nth 1 *posix-argv*))))
+							(< 0 (ignore-errors (parse-integer (nth 2 *posix-argv*))))
+							)))
+					 (print *usage-string*) ; Raise error
+					 (main *posix-argv*) ; Go on
+				 ))
+
+(defun main (argv)(
+				   let ()
+					;; SDL stuff
+					(sdl:with-init ()
+					  (sdl:window width height :title-caption "Carniflex")
+					  (sdl:update-display)
+
+					  (sdl:with-events ()
+						(:quit-event () t)
+						(:key-down-event (:key key) (handle-key key))
+						(:idle ()
+							   (sdl:clear-display sdl:*black*)
+							   (game 0)
+							   (sdl:update-display)
+							   (sleep 1)
+							   )
+						))
+					))
 
 (sb-int:with-float-traps-masked (:invalid :inexact :overflow)
-(main *posix-argv*))
+  (parse))
