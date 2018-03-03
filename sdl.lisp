b814b396b4cfcb0b2eaa3d5bee0f664160accee6
Anis GADHGADHI <agadhgad@e2r10p1.42.fr>
2018-03-03 18:52:58 +0100

    update output

--- sdl.lisp
+++ sdl.lisp
@@ -6,21 +6,46 @@
 	(setq size (* size zoom))
 ))
 
+(defun speedDown () (
+	if (= IMAGE_PER_SEC 1)
+		(format t "can't speed less ~%")
+		(
+		let ()
+		(setq IMAGE_PER_SEC (- IMAGE_PER_SEC 1))
+		(setf (sdl:frame-rate) IMAGE_PER_SEC)
+		)
+))
+
+(defun speedUp () (
+	if (= IMAGE_PER_SEC 50)
+		(format t "can't speed more ~%")
+		(
+		let ()
+		(setq IMAGE_PER_SEC (+ IMAGE_PER_SEC 1))
+		(setf (sdl:frame-rate) IMAGE_PER_SEC)
+		)
+))
+
 ;;handle key events
 (defun handle-key (key)
 	(when (SDL:KEY= KEY :SDL-KEY-ESCAPE) (SDL:PUSH-QUIT-EVENT))
 	(when (SDL:KEY= KEY :SDL-KEY-a) (zoomIn))
+	(when (SDL:KEY= KEY :SDL-KEY-left) (speedDown))
+	(when (SDL:KEY= KEY :SDL-KEY-right) (speedUp))
 	;;check keys here: https://gitlab.com/dto/xelf/blob/master/keys.lisp
 )
 
 ;;DRAW on screen
 (defun draw (y x kind) (
 	let (
-		(sx (* zoom (* x size)))
-		(sy (* zoom (* y size)))
+		(sx (+ (* zoom (* x size)) margin))
+		(sy (+ (* zoom (* y size)) margin))
 	)
 	(sdl:draw-box
-		(sdl:rectangle-from-edges-* sx sy (+ sx size) (+ sy size))
+		(sdl:rectangle-from-edges-* sx sy
+			(- (+ sx size) margin)
+			(- (+ sy size) margin)
+		)
 		:color (if (= kind DEAD) COLOR_DEAD COLOR_ALIVE)
 	)
 ))
--- variables.lisp
+++ variables.lisp
@@ -8,12 +8,14 @@
 (defvar N 16)
 (defvar M 16)
 
-(defvar COLOR_ALIVE (sdl:color :r 255 :g 255 :b 255)) ;;white
+(defvar COLOR_ALIVE (sdl:color :r 255 :g 0 :b 0)) ;;red
 (defvar COLOR_DEAD (sdl:color :r 0 :g 0 :b 0)) ;;black
+(defvar COLOR_BACKGROUND (sdl:color :r 255 :g 255 :b 255)) ;;white
 
 (defvar IMAGE_PER_SEC 1)
 
 (defvar offX 0)
 (defvar offY 0)
 (defvar zoom 1.0)
+(defvar margin 1)
 (defvar size (/ WIDTH 16))
